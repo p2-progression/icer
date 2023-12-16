c2c8c43c69232f80b9428d3a4953a228c5023e36
Adenohitu <49752462+adenohitu@users.noreply.github.com>
2023-12-17 01:55:14 +0900

    feat:Shutterの実装

--- src/app/components/shutter.tsx
+++ src/app/components/shutter.tsx
@@ -19,11 +19,6 @@ function LinearProgressWithLabel(
       <Box component="div" sx={{ width: "100%", mr: 1 }}>
         <LinearProgress variant="determinate" {...props} />
       </Box>
-      {/* <Box component="div" sx={{ minWidth: 35 }}>
-        <Typography variant="body2" color="text.secondary">{`${Math.round(
-          props.value
-        )}%`}</Typography>
-      </Box> */}
     </Box>
   );
 }
